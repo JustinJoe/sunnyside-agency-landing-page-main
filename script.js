0859a492886a32a848c63ba1b3ef197ad319c26c
JustinJoe <josephjustin.om@gmail.com>
2021-06-16 23:28:22 +0000

    mobile nav complete. working on media queries

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const menu = document.querySelector(".hamburger-menu");
+const welcome = document.querySelector(".welcome");
 const nav = document.querySelector(".nav");
 console.dir(nav);
 
 function displayHandler() {
-  nav.classList.toggle("show");
+  nav.classList.toggle("visible");
+  welcome.classList.toggle("invisible");
 }
 
 menu.addEventListener("click", displayHandler);
